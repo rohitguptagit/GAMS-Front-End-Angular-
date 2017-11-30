555a254b231ede2c4e72e547aec6616fc5c593dc
Rohit Gupta <rohit.gupta2@mail.mcgill.ca>
2017-11-30 01:04:51 -0500

    Final commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+@Component({
+  template: `
+  <br><br>
+    <img src = "../../assets/images/logo.jpeg" alt="Simple Admin logo" style="height: 525px; width: 1050px">
+  `,
+})
+export class HomeComponent implements OnInit {
+
+
+  ngOnInit(){
+
+  }
+ 
+}
---
+++ src/app/home/home.routes.ts
@@ -0,0 +1,9 @@
+// Imports
+import { Routes } from '@angular/router';
+
+import { HomeComponent }   from './home.component';
+
+// Route Configuration
+export const homeRoutes: Routes = [
+  { path: 'home', component: HomeComponent }
+];
